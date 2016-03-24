1711bdfb6a4be5eec74769c220b5a5f347c02abb
Flávio Spinelli <flavio@dizain.com.br>
2016-03-24 17:01:22 -0300

    .

--- wp-content/themes/pestana/header.php
+++ wp-content/themes/pestana/header.php
@@ -12,7 +12,21 @@
 	</head>
 <body>
 	<header>
-		<nav>
-			<?php wp_nav_menu(['menu_class'=>'', 'container'=>'ul']); ?>
+		<nav class="navbar navbar-inverse">
+		  <div class="container">
+		  	<div class="container-fluid">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span> 
+		      </button>
+		      <a class="navbar-brand" href="#">WebSiteName</a>
+		    </div>
+		    <div class="collapse navbar-collapse" id="myNavbar">
+		      <?php wp_nav_menu(['menu_class'=>'nav navbar-nav navbar-right', 'container'=>'ul']); ?>
+		    </div>
+		  </div>
+		  </div>
 		</nav>
 	</header>
